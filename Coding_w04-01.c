1a89000eb375b3ddda80585e9f99f3201058a4c0
ACHIRAWATs6812032610215 <s6812032610215@email.kmutnb.ac.th>
2025-07-25 12:28:33 +0700

    Add files via upload

---
+++ Coding_w04-01.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Keyword + Header file
+
+int main() { // Keyword: int, Identifier: main, Operator: ()
+    printf("Hello,\n");       // Keyword: printf, Constant: "Hello,\n"
+    printf("C Tokens!\n");    // Constant: "C Tokens!\n"
+    return 0;                 // Keyword: return, Constant: 0
+}
